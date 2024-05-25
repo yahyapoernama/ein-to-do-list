c2edf998e63a92bc44e5f5ab49d1a60fbfb98e92
Mochamad Yahya Dwi Purnama <yahyapoernama@gmail.com>
2024-05-26 02:08:26 +0700

    chore: Import Template (Qash) and Styling Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,77 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Qash - Crypto Exchange Dashboard UI Kit</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('vendor/qash/images/favicon.png') }}" />
+    <!-- Custom Stylesheet -->
+    <link rel="stylesheet" href="{{ asset('vendor/qash/css/style.css') }}" />
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<body class="{{ Session::get('theme') }}">
+    <div id="preloader"><i>.</i><i>.</i><i>.</i></div>
+
+    <div id="main-wrapper p-0">
+        <div class="authincation section-padding">
+            <div class="container h-100">
+                <div class="row justify-content-center h-100 align-items-center">
+                    <div class="col-xl-5 col-md-6">
+                        <div class="mini-logo text-center my-4">
+                            <a href="./intro.html"><img src="{{ asset('vendor/qash/images/logo.png') }}" alt="" /></a>
+                            <h4 class="card-title mt-3">Sign in to Qash</h4>
+                        </div>
+                        <div class="auth-form card">
+                            <div class="card-body">
+                                <form class="signin_validate row g-3" action="otp-2.html">
+                                    <div class="col-12">
+                                        <input type="email" class="form-control" placeholder="hello@example.com"
+                                            name="email" />
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="password" class="form-control" placeholder="Password"
+                                            name="password" />
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                                            <label class="form-check-label" for="flexSwitchCheckDefault">Remember me</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 text-end">
+                                        <a href="reset.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary btn-block">
+                                            Sign in
+                                        </button>
+                                    </div>
+                                </form>
+                                <p class="mt-3 mb-0">
+                                    Don't have an account?
+                                    <a class="text-primary" href="signup.html">Sign up</a>
+                                </p>
+                            </div>
+                        </div>
+                        {{-- <div class="privacy-link">
+                            <a href="signup.html">Have an issue with 2-factor authentication?</a>
+                            <br />
+                            <a href="signup.html">Privacy Policy</a>
+                        </div> --}}
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <script src="{{ asset('vendor/qash/js/scripts.js') }}"></script>
+    <script></script>
+</body>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
