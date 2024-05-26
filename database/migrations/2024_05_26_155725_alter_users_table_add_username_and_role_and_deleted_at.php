1f8ad194543ca452487ba478b1fbb67a5ed1015e
Mochamad Yahya Dwi Purnama <yahyapoernama@gmail.com>
2024-05-26 22:58:45 +0700

    chore: Add Migration (Alter Table users, Add Column username, role, and deleted_at)

---
+++ database/migrations/2024_05_26_155725_alter_users_table_add_username_and_role_and_deleted_at.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->nullable()->after('name');
+            $table->string('role')->nullable(false)->default('User')->after('email');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('role');
+            $table->dropColumn('deleted_at');
+        });
+    }
+};
