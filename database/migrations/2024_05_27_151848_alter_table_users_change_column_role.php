dc1af5443c03d1a8e5cefb6a38876ed603fc7f83
Mochamad Yahya Dwi Purnama <yahyapoernama@gmail.com>
2024-05-27 22:21:42 +0700

    chore: Add Migration Alter Table users (Change role column to enum)

---
+++ database/migrations/2024_05_27_151848_alter_table_users_change_column_role.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['Superadmin', 'Admin', 'User'])->default('User')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->nullable(false)->default('User')->change();
+        });
+    }
+};
